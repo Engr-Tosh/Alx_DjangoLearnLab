9f62e2597458625aed986e6f6dda0ec2125ef289
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-21 14:47:23 +0100

    Added models showcasing django orm understanding

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,4 +2,7 @@ import relationship_app.models
 
 
 #query all books by a specific author, that is to get all books by a particular author.
-books = 
+
+#list all books in the library
+books = Library.objects.get(name=library_name)
+books.all()
