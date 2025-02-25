c05286598040598ec16cce1a2a8c785cd6856a35
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-25 03:53:46 +0100

    Added basic user authenticarion to the relationship app

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'WAT'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import path, reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 #The first task is to list all the books stored in the database
@@ -9,17 +12,22 @@ from django.views.generic.detail import DetailView
 
 def list_books(request):
     """This function will retrieve all books and render it via a template displaying the list"""
-
     books = Book.objects.all()  #Fetches all book instances from the db
     context = {'book_list': books}  #Creates a dict to hold the list with key book_list
     return render(request, 'relationship_app/list_books.html', context)
 
 #Now to implement a class based view
 #class based view that displays details for a specific library and lists all the books in that library
-
 class LibraryDetailView(DetailView):
     """A class to display the details of a specific book"""
     model = Library
     template_name = 'relationship_app/library_detail.html'
-   
-    
+
+
+"""Implementing user authentication views in django"""
+#Handles User registration
+class RegistrationView(CreateView):
+    form_class = UserCreationForm       #Creates a view class for user registration
+    success_url = reverse_lazy('login')     #Page to redigrect to after the registration is successful
+    template_name = 'registration/register.html'
+
