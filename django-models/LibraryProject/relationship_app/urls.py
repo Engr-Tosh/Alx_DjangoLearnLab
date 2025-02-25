7cff9667aaa3df1e357a6d0ae0c78312207cd205
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-25 04:44:07 +0100

    Added basic user authenticarion to the relationship app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('library_details/', views.LibraryDetailView.as_view(), name= 'details'),
     path('register/', views.register.as_view(), name = 'register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name ='login'),
-    path('logout', LogoutView.as_view(template_name="registration/logout.html"), name='logout')
+    path('logout', LogoutView.as_view(template_name="registration/logout.html"), name='view.register')
 ]
