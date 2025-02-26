5165001e4a7a7a6a66feb5bd2c413bb3b212543a
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-26 04:03:42 +0100

    omo

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('library_details/<int:pk>/', views.LibraryDetailView.as_view(), name= 'details'),
     path('register/', views.register.as_view(), name = 'register'),
     path('login/', views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path('logout/', views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
-    # path('logout_page/', views.logout, name='logout_page')
+    # path('logout/', views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
+    path('logout_page/', views.logout, name='logout_page')
  
 ]
