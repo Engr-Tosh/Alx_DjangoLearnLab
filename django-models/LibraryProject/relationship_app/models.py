387055437d8f13a2045c12cd4366a92cfd221a73
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-27 14:02:47 +0100

    Added a user creation model and signals

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -25,3 +26,31 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+
+"""Implementing role based access in django"""
+"""1.Extend the user Model with with a UserProfile"""
+class UserProfile(models.Model):
+    #Each user role is to be given a choice.
+    ROLE_CHOICES = {
+        ('admin', 'Admin'),
+        ('member', 'Member'),
+        ('librarian', 'Librarian')
+    }
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(choices=ROLE_CHOICES, default='member')
+
+    def __str__(self):
+        return self.user 
+    
+"""Using django signals for automatic creation"""
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+"""Signal creation"""
+@receiver(post_save, sender=UserProfile)        #signals that the trigger would be from this sender
+def user_profile_created(sender, instance, created, **kwargs)
+    if created:
+        UserProfile.objects.create(instance)
+        return f"UserProfile automatically created for {instance}"
