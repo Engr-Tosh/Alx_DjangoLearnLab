690db967248649dbbe1ea4947b6ed963fc4a217e
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-28 23:11:38 +0100

    Added urls for custom permissions views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path('admin/', views.Admin, name = "admin_page"),
-    path('librarian/', views.Librarian, name="librarians_page")
+    path('librarian/', views.Librarian, name="librarians_page"),
+    path('add_book/', views.add_book, name = "add_book"),
+    path('delete_book', views.delete(), name="delete_book"),
+    path('update_book', views.edit(), name="update_book")
 ]
