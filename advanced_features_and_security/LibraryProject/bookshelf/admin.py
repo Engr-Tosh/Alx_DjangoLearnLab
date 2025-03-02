f1215ef82146ef60d1cbcfc36671a21b7c19e373
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-02 23:45:36 +0100

    registered the custom models to the admin panel

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
@@ -10,3 +11,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, UserAdmin)
