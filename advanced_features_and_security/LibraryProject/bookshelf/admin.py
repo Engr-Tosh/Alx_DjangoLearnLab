d8c884d36d252af6747118c23b9e5e403762dc55
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-03 04:22:43 +0100

    registered the custom models to the admin panel

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -9,6 +9,12 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author')
     search_fields = ('title', 'author')
 
+"""CustomAdmin class creation"""
+class ModelAdmin(UserAdmin):
+    """Configurations for additional fields"""
+    list_display = ('email', 'username', 'password', 'date_of_birth', 'profile_photo')
+    list_filter = ('email', 'username')
+    search_fields = ('email', 'username')
 
 admin.site.register(Book, BookAdmin)
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, ModelAdmin)
