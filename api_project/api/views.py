12240e44ff76fd8b6002e293ed4ffda242e0fba0
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-04 08:35:22 +0100

    Added API view for the retrieval of model records

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
 """Creating the view for my first API Endpoint"""
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
+#Now to define the view
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    
