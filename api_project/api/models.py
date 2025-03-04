7bd87a5fcdabe096249ca617192a3bbfd6864400
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-04 07:34:54 +0100

    Updated the model fields

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 """Creating a basic model for my api"""
 class Book(models.Model):
-    title = models.CharField
-    author = models.CharField
+    title = models.CharField(max_length=255, default="Unknown")
+    author = models.CharField(max_length=255, default="Unknown")
