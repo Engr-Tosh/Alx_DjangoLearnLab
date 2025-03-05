d66c57b6801cb5613f2211729af0490966c26c14
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-05 18:13:59 +0100

    Added router to auto determine url config

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,18 @@
 #App's "api" Urls go here
 
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+#register the BookViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 #include the path to the book list api view 
 urlpatterns = [
+    #Route for Booklist view
     path("books/", BookList.as_view(), name="book-list"),
+
+    #router for the BookViewSet(all CRUD operations)
+    path('', include(router.urls))
 ]
