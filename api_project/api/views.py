b79c616fc58eb536c9d6323b5c1f997d8e506300
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-05 17:31:14 +0100

    Added a viewset to simplify management of db operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 """Creating the view for my first API Endpoint"""
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -10,4 +10,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    
+"""Adding a viewset that would extend the existing BookList view"""
+class BookViewSet(BookList, viewsets.ModelViewSet):
+    pass
