627117fceb44ff851d000c346611962a1418c91f
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-06 08:23:10 +0100

    Added custom usermanager for the custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from datetime import date
 
 # Create your models here.
@@ -16,3 +16,25 @@ class Book(models.Model):
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(default=date.today, null=True)
     profile_photo = models.ImageField(default="default.jpg", null=True)
+
+    """Implementing custom user manager for the custom user model"""
+class CustomUserManager(BaseUserManager):
+    #Custom usermanager methods 
+
+    def create_user(self, email, username=None, password=None, **extra_fields):
+        """Create and return a normal user with username and password"""
+        if not email:
+            raise ValueError("Email field is required")
+        
+        email = self.normalize_email(email)     #converts the email string into lowercase
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password) #This hashes the password
+        user.save(using=self._db)   #save to this paricular database incase of the use multiple databases
+        return user
+    
+    """Creating and returning a superuser"""
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, username, password, **extra_fields)
