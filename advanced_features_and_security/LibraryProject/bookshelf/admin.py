f075387df9d77347866cb6c7212ac8abdd2b4de9
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-06 09:01:52 +0100

    asssigned customusermanager, Registered customuseradmin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 
 # Register your models here.
 
@@ -8,5 +8,14 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author')
     search_fields = ('title', 'author')
 
+"""Integrating custom user model into admin by adding 
+custom Model admin class that includes config 
+for the additional field os the model"""
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('date_of_birth', 'profile_photo')
+
 
 admin.site.register(Book, BookAdmin)
+
+#Register the the custom user admin
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from datetime import date
+from django.conf import settings    #The AUTH USER MODEL setting will be imported here
 
-# Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200, default="Unknown")
     author = models.CharField(max_length=100, default="Unknown")
     publication_year = models.IntegerField(default=2024)
+
+    #To ensure the association between model and user
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
@@ -16,6 +19,8 @@ class Book(models.Model):
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(default=date.today, null=True)
     profile_photo = models.ImageField(default="default.jpg", null=True)
+
+    objects = CustomUserManager
 
     """Implementing custom user manager for the custom user model"""
 class CustomUserManager(BaseUserManager):
@@ -38,3 +43,6 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
 
         return self.create_user(email, username, password, **extra_fields)
+    
+#assigning the usermanager to the custom user
+CustomUser.objects = CustomUserManager()
