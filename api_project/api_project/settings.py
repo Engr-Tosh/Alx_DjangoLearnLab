83dec2acf5a7127a1f0caad1a900d260e4e2a9a4
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-09 08:57:57 +0100

    Configured token based authentication

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'rest_framework.authtoken', 
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Add Django Rest framework Authentiacation classes
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',    #adds token based auth
+        'rest_framework.authentication.SessionAuthentication',      #session/web based autentication
+    ) 
+}
