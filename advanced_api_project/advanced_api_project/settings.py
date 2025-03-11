3a1ab14784e06fc07e5ab031196c2a505cfc9564
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-11 13:14:44 +0100

    Added a new drf project

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)ynrdlx^p=#s3y4_$0p^42a@*r($$x5c2r-7i%8j#$dgoftry'
+SECRET_KEY = 'django-insecure-oimb$tvxq!2lt$^cr4&^=c8-af6e3wn(g-!o7)0*-+lmo#xa_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
