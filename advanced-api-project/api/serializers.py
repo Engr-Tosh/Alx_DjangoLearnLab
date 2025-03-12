38fcdc29205c7e3c3523ff5bad6218e1eeab26e2
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-12 07:03:29 +0100

    Added a serializer for a model

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,3 +1,10 @@
 #serializers go here
 
-from dj
+from rest_framework import serializers
+from .models import Book
+
+#Book model serializer
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "publication_year"]
