c57f9c19dd70585a0bf166e2a6825fc744644231
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-14 05:34:22 +0100

    Added an endpoint to list/retrieve model instances

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Having created my book model
+#To implement generic view endpoints for performing CRUD operations on the model
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer 
+
+#ListView endpoint for retrieving all books
+
+class BookListAPiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
