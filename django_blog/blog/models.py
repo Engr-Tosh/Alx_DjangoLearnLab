cd88f6237cb2b5c03b4575734563927b64dc1279
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-19 04:09:41 +0100

    Adding a new model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Create your models here
+# Tables/Models for my blog application
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, default="")
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post-author")
