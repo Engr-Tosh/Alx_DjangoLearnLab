0e12094a7bea089db17552d4b3f9f6088c1b903f
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-19 04:44:41 +0100

    Adding static files and configurations

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
     title = models.CharField(max_length=200, default="")
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post-author")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_author")
