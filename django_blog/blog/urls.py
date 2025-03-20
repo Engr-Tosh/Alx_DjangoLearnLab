290f221b030174d175df2e02acd6b6e21fba7044
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-20 02:11:00 +0100

    Adding auth components and routes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView, LogoutView
 from django.contrib.auth.views import LoginView
+from . import views
 
 
 urlpatterns = [
     path("register/", SignUpView.as_view(), name="register"),
-    path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"), 
+    path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="blog/logout.html"), name="logout-user"),
+    path("profile/", views.profile_view, name="profile")
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.views import View
@@ -14,4 +15,27 @@ class SignUpView(CreateView):
     template_name = 'blog/register.html'        #template to use for registration
 
 
-#Implementing the login viewvia CBV which is called from Django built in views
+#Implementing the logout view via CBV which is called from Django built in views
+class LogoutView(View):
+    template_name=""
+
+    """Logout view would display this view when the user logs out"""
+    def get(self, request):     #Remember a view takes an http request and returns a response
+        logout(request)         #Logs Out a User
+        return render(request, self.template_name)      #Returns this page after a user is logged out.
+    
+        
+#Implementing the user profile view
+def profile_view(request):
+    """Gets the user profile and display the profile details"""
+    user = request.user
+    context = {"user": user}
+    return render(request, "blog/profile.html", context)
+
+
+"""Step 4: Implement Profile Management
+Profile Management Features:
+Develop a view that allows authenticated users to view and edit their profile details. This view should handle POST requests to update user information.
+Ensure the user can change their email and optionally extend the user model to include more fields like a profile picture or bio."""
+
+#View that allows authenticated users to edit profile details
