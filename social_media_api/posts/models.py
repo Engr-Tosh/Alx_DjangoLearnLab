2fad228d61115d89ed54990a1afbd2970b18df86
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-28 17:39:37 +0100

    Made code correcttions for the model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, related_name="posts")
     title = models.CharField(max_length=255, blank=True, null=True)
-    content = models.TextField(blank=True, null=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
