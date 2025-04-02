8f70bdf04346fffa34fef270645ea1c7dfbdefd1
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-04-02 06:43:24 +0100

    Added route for follow management

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,13 +3,16 @@ from django.urls import path
 from .views import (
     UserRegisterView,
     UserLoginView,
-    UserProfileView
+    UserProfileView,
+    FollowersAPIView
 )
-from rest_framework.authtoken import views
+
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
     path('login/', UserLoginView.as_view(), name="login"),
-    path('profile/', UserProfileView.as_view(), name="profile")
+    path('profile/', UserProfileView.as_view(), name="profile"),
+    path('follow/<int:user_id>/', FollowersAPIView, name='follow'),
+    path('unfollow/<int:user_id>', FollowersAPIView.as_view(), name="unfollow")
 
 ]
