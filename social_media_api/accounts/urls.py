5d30fa57485862c2be8056a7a4e7a8a5c3a99edd
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-04-02 06:44:04 +0100

    Added route for follow management

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
     path('profile/', UserProfileView.as_view(), name="profile"),
     path('follow/<int:user_id>/', FollowersAPIView, name='follow'),
-    path('unfollow/<int:user_id>', FollowersAPIView.as_view(), name="unfollow")
+    path('unfollow/<int:user_id>/', FollowersAPIView.as_view(), name="unfollow")
 
 ]
