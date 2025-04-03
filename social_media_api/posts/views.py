8c4e2c36a73ea9b73fedb6139afe790da8c47c5b
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-04-03 13:25:20 +0100

    Code modifications, Added like/unlike functionality with notifications on like action only

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,14 @@
-from rest_framework import permissions, pagination, viewsets, generics
-from .models import Post, Comment
+from rest_framework import permissions, pagination, viewsets, generics, status
+from rest_framework.response import Response
+from .models import Post, Comment, Like
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     PostSerializer,
     CommentSerializer,
 )
 from rest_framework.filters import SearchFilter
+from notifications.models import Notification 
+
 # Applying pagination
 class PostCommentPagination(pagination.PageNumberPagination):
     page_size = 10
@@ -24,7 +27,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
-    pagination_class = [PostCommentPagination]
+    pagination_class = PostCommentPagination
     filter_backends = [SearchFilter]
     search_fields = ["author"]
 
@@ -40,4 +43,53 @@ class PostFeedView(generics.ListAPIView):
         following_users = self.request.user.following.all()  # Fetches all the users being followed
         return Post.objects.filter(author__in=following_users).order_by("-created_at")   # Shows the most reent posts of those users 
 
-        
+# View to handle a user liking a post
+class LikePostAPIView(generics.GenericAPIView):
+    """Handles when a user likes a post"""
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request, *args, **kwargs):
+        """
+        Like a post
+        """
+
+        pk = request.data.get("post_pk")
+
+        if not pk:
+            return Response({"error": "Post ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        post = generics.get_object_or_404(Post, pk=pk)
+
+        like_instance, created = Like.objects.get_or_create(user=request.user, post=post)
+
+        if created:
+            Notification.objects.create(
+                actor= request.user,
+                recipient= post.user,
+                verb= "like",
+                target = post
+            )
+            return Response({"Message": "Liked a post",}, status=status.HTTP_201_CREATED)
+
+        return Response({"message": "You have already liked this post"}, status=status.HTTP_400_BAD_REQUEST)
+
+# When a user unlikes a post
+class UnlikePostAPIView(generics.GenericAPIView):
+    """
+    Unlikes a post
+    """
+    def post(self, request, *args, **kwargs):
+        pk = request.data.get("post_pk")
+
+        if not pk:
+            return Response({"error": "Post ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        post = generics.get_object_or_404(Post, pk=pk)     
+
+        like_instance = Like.objects.filter(user=request.user, post=post).first()
+
+        if like_instance:
+            like_instance.delete()
+            return Response({"Message": "Post unliked succesfully"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "You have not liked this post"}, status=status.HTTP_400_BAD_REQUEST)
